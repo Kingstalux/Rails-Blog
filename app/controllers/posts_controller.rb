82eaf01223cc79da9634abfdf45243f20a45d435
Kingstalux <kingsleyngu25@gmail.com>
2022-02-18 05:55:18 +0100

    Fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ActionController::Base
-    def index
-    end
-    def show
-    end
+  def index; end
+
+  def show; end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ActionController::Base
-    def index
-    end
-    def show
-    end
+  def index; end
+
+  def show; end
 end
