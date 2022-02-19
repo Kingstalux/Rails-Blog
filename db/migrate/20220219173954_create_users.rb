50d602c4cf5e26102920c53a2d927f5e1e1f1f21
Kingstalux <kingsleyngu25@gmail.com>
2022-02-19 18:42:00 +0100

    Add users table

---
+++ db/migrate/20220219173954_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :Name
+      t.string :Photo
+      t.string :Bio
+      t.date :UpdatedAt
+      t.date :CreatedAt
+      t.integer :PostsCounter
+
+      t.timestamps
+    end
+  end
+end
