4ab197f3111357cd9a9ef83bca122c5e0a022597
Kingstalux <kingsleyngu25@gmail.com>
2022-02-19 18:54:31 +0100

    Add post table

---
+++ db/migrate/20220219174950_create_post.rb
@@ -0,0 +1,14 @@
+class CreatePost < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :Title
+      t.string :Text
+      t.date :CreatedAt
+      t.date :UpdatedAt
+      t.integer :CommentsCounter
+      t.integer :LikesCounter
+
+      t.timestamps
+    end
+  end
+end
