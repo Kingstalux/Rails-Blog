808040d10540fd29fb95d56493d8868fdae5b3a4
Kingstalux <kingsleyngu25@gmail.com>
2022-02-19 20:30:06 +0100

    Migrated tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_19_192250) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_19_192902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_19_192250) do
     t.date "CreatedAt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "post_id", null: false
+    t.integer "PostId"
+    t.bigint "user_id", null: false
+    t.integer "AuthorId"
+    t.index ["AuthorId"], name: "index_comments_on_AuthorId"
+    t.index ["PostId"], name: "index_comments_on_PostId"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -27,6 +35,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_19_192250) do
     t.date "UpdatedAt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "post_id", null: false
+    t.integer "PostId"
+    t.bigint "user_id", null: false
+    t.integer "AuthorId"
+    t.index ["AuthorId"], name: "index_likes_on_AuthorId"
+    t.index ["PostId"], name: "index_likes_on_PostId"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -37,6 +53,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_19_192250) do
     t.integer "LikesCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.integer "AuthorId"
+    t.index ["AuthorId"], name: "index_posts_on_AuthorId"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,4 +70,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_19_192250) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
