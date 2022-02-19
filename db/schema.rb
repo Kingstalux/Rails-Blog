5e3485875ff1fccee7bd3da7e7140ae0c8fa83af
Kingstalux <kingsleyngu25@gmail.com>
2022-02-19 20:25:35 +0100

    Migrated tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_19_191425) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_19_192250) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "Text"
+    t.date "UpdatedAt"
+    t.date "CreatedAt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.date "CreatedAt"
+    t.date "UpdatedAt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "Text"
+    t.date "CreatedAt"
+    t.date "UpdatedAt"
+    t.integer "CommentsCounter"
+    t.integer "LikesCounter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "Name"
