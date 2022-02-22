418551a974ea39490e541163b994d09312d98580
Kingstalux <kingsleyngu25@gmail.com>
2022-02-22 11:30:41 +0100

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    def self.current_user
-        User.first
-    end 
+  def self.current_user
+    User.first
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,15 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: "User", foreign_key: "user_id"
-    has_many :comments, class_name: "Comment", foreign_key: "post_id"
-    has_many :likes, class_name: "Like", foreign_key: "post_id"
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
 
-    def self.recent_comments(id)
-        Comment.where(post_id: id).limit(5)
-    end
+  def self.recent_comments(id)
+    Comment.where(post_id: id).limit(5)
+  end
 
-    def self.count_post(id)
-        user = User.find(id)
-        user.postsCounter = Post.where(user_id: id).count
-        user.save
-    end
+  def self.count_post(id)
+    user = User.find(id)
+    user.postsCounter = Post.where(user_id: id).count
+    user.save
+  end
 end
