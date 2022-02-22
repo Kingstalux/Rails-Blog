fca6db33861a9e976ff41f32b2f000c9d3693e3c
Kingstalux <kingsleyngu25@gmail.com>
2022-02-22 08:52:16 +0100

    Add associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :user
+    belongs_to :author, class_name: "User", foreign_key: "user_id"
+    has_many :comments, class_name: "Comment", foreign_key: "post_id"
+    has_many :likes, class_name: "Like", foreign_key: "post_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
-    has_many :likes
+    has_many :posts, class_name: "Post", foreign_key: "user_id"
+    has_many :comments, class_name: "Comment", foreign_key: "user_id"
+    has_many :likes, class_name: "Like", foreign_key: "user_id"
 end
