6003ed021385b24542f1d3edb9b3a3a7601b9fdd
Kingstalux <kingsleyngu25@gmail.com>
2022-02-26 11:20:14 +0100

    Fix linters

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
-    
-    before {subject.save}
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
 
-    it 'name should be present' do
-        subject.name = nil
-        expect(subject).to_not be_valid  
-    end
+  before { subject.save }
 
-    it 'postsCounter should be greater than or equal to zero' do
-        subject.postsCounter = -1
-        expect(subject).to_not be_valid  
-    end
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'postsCounter should be greater than or equal to zero' do
+    subject.postsCounter = -1
+    expect(subject).to_not be_valid
+  end
 end
