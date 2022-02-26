43c610884d05a4d8df0d9b2efcf8ed984ce595c8
Kingstalux <kingsleyngu25@gmail.com>
2022-02-26 11:12:13 +0100

    Solve n+1 problem

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-    first_user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-    subject { Post.new(author: first_user, title: 'Hello', text: 'This is my first post') }
+    first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    subject { Post.create(author: first_user, title: 'Hello', text: 'This is my first post') }
     
     before {subject.save}
 
@@ -25,4 +25,5 @@ RSpec.describe Post, type: :model do
         subject.likesCounter = -1
         expect(subject).to_not be_valid  
     end
+
 end
