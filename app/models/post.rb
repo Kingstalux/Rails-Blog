9fe414e9d574724540f63cc4b46b242469371638
Kingstalux <kingsleyngu25@gmail.com>
2022-03-03 11:33:41 +0100

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
-  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id', dependent: :destroy
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id', dependent: :destroy
 
   validates :title, presence: true, length: { in: 1..250 }
   validates :likesCounter, numericality: { greater_than_or_equal_to: 0 }
